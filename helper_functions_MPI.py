775dd12d03752c58f65a70ee3539b1b74d608e83
Daniel-Stedman <daniel.stedman@mail.utoronto.ca>
2024-08-22 13:28:54 -0700

    Added comments to each helper function

--- helper_functions_MPI.py
+++ helper_functions_MPI.py
@@ -7,6 +7,8 @@ import sys
 def pairfieldN2(L, h1, wlayer):
     """
     Creates the weather matrix for the storms, S_st in paper.
+
+    Calculates the subsidence correction.
     """
     
     voldw = np.sum(wlayer) * dx**2
@@ -44,35 +46,34 @@ def viscND(vel, Re, n):
 ### New pairshapeN2 function. Generates Gaussians using entire domain instead of creating sub-domains. (Daniel) ###
 def pairshapeN2(locs, t, x, y, offset):
 
-    wlayer = np.zeros_like(x).astype(np.float64)
+    wlayer = np.zeros_like(x).astype(np.float64) # Initializes the weather layer
 
-    resolution = round(np.sqrt(1/Br2)/dx)
-    padding = 3*resolution
+    resolution = round(np.sqrt(1/Br2)/dx) # The number of grid points per storm
+    padding = 3*resolution # The padding outside of which the gaussian will be terminated to 0
 
-    #print(round(len(x)/2))
 
-    xcenter = x[round(offset/2), round(offset/2)]
+    xcenter = x[round(offset/2), round(offset/2)] # x,y center of the current subgrid (used for MPI)
     ycenter = y[round(offset/2), round(offset/2)]
 
     for i in range(len(locs)):
+        #Checking if the current storm placement is close enough to or within the current subgrid and calculates its contribution
         if np.abs(locs[i][0] - xcenter) < len(x)/2 + padding and np.abs(locs[i][1] - ycenter) < len(y)/2 + padding:
+            # Check if storm is dormant, tstf < lifetime < tstpf
             if (t-locs[i][-1]) <= locs[i][2] or t == 0:
-                #xloc = locs[i][0]
-                #yloc = locs[i][1]
-                #zonex = x[yloc-padding:yloc+padding, :][:, xloc-padding:xloc+padding]
-                #zoney = y[yloc-padding:yloc+padding, :][:, xloc-padding:xloc+padding]
-                layer = Wsh * np.exp( - (Br2*dx**2)/0.3606 * ( (x-locs[i][0])**2 + (y-locs[i][1])**2))
-                #wlayer[yloc-padding:yloc+padding, :][:, xloc-padding:xloc+padding] += layer
+
+                layer = Wsh * np.exp( - (Br2*dx**2)/0.3606 * ( (x-locs[i][0])**2 + (y-locs[i][1])**2)) # Adds the storm to the weather layer
                 wlayer += layer
 
     return wlayer
 
+
 def BernN2(u1, v1, u2, v2, gm, c22h, c12h, h1, h2, ord):
     """
-    Bernoulli
+    Bernoulli 
     """
     B1 = c12h * h1 + c22h * h2 + 0.25 * (u1**2 + u1[:,r]**2 + v1**2 + v1[r,:]**2)
 
+    # Allows one to use the old bugged version of the code. Used for testing, actual simulations should always have fixed == True in name_list.
     if fixed == False:
         B2 = gm * c12h * h1 + c22h * h2 + 0.25 * (u1**2 + u1[:,r]**2 + v1**2 + v1[r,:]**2)
     else:
@@ -80,23 +81,12 @@ def BernN2(u1, v1, u2, v2, gm, c22h, c12h, h1, h2, ord):
 
     return B1, B2
 
-def xflux(f, u):  # removed dx, dt from input
-    fl = f[:,l]
-    fr = f
 
-    fa = 0.5 * u * (fl + fr)
-
-    return fa
-
-def yflux(f, v):  # removed dx, dt from input
-    fl = f[l,:]
-    fr = f
-
-    fa = 0.5 * v * (fl + fr)
-
-    return fa
 
 def calculate_KE(u1,u2,v1,v2,h1,h2, H1H2, p1p2):
+    """
+    Calculates the kinetic energy of a configuration of the simluation. These equations can be found in O'Neill et al. (2016).
+    """
     first = p1p2*H1H2*h1*(u1**2 + v1**2)
     second = h2*(u2**2 + v2**2)
 
@@ -104,6 +94,9 @@ def calculate_KE(u1,u2,v1,v2,h1,h2, H1H2, p1p2):
 
 
 def calculate_APE(h1, h2, H1H2, p1p2, c12h, c22h):
+    """
+    Calculates the available potential energy of a configuration of the simulation. The equations can be found in O'Neill et al. (2016).
+    """
     first = 0.5*p1p2*H1H2*c12h*(h1-1)**2
     second = 0.5*c22h*(h2-1)**2
     third = p1p2*H1H2*(c22h/c12h)*c12h*(h1-1)*(h2-1)
@@ -117,18 +110,22 @@ def genlocs(num, N, t):
     Generates a list of coordinates, storm duration, storm period, and tclock.
 
         - Made it more pythonic and faster - D
+
+    The "locs" list contains information about each storm present in the simulation, and all the information they need.
     """
     
-    choices = np.random.randint(0, len(poslocs), num)
+    choices = np.random.randint(0, len(poslocs), num) 
 
-    locs = poslocs[choices]
+    locs = poslocs[choices] # make random choices of loction from all possible storm locations (not in the sponge layer)
     
-    newdur = np.round(np.random.normal(tstf, 2, (num, 1)))
-    newper = np.round(np.random.normal(tstpf, 2, (num, 1)))
+    newdur = np.round(np.random.normal(tstf, 2, (num, 1)))  # Pick durations for the storms from a normal distribution
+    newper = np.round(np.random.normal(tstpf, 2, (num, 1))) # Pick periods for the storms from a normal distribution
 
-    final = np.append(locs, newdur, axis=1)
-    final = np.append(final, newper, axis=1)
+    final = np.append(locs, newdur, axis=1) 
+    final = np.append(final, newper, axis=1) # Create the final array of all storms.
 
+
+    # Adding the current time so that the storm knows when it was created.
     if t == 0:
         final = np.append(final, np.round(np.random.normal(0, tstf, (num,1))), axis=1).astype(np.int64)
     else:
@@ -136,11 +133,18 @@ def genlocs(num, N, t):
 
     return final
 
-### New helper function for MPI ###
+
+### New helper functions for MPI ###
+
 def split(arr, offset, ranks, rank):
+    """
+    Splits an array in to subarrays based on the number of ranks. It all wraps the subarrays with period boundary conditions on the
+    main array. The subarrays have a padding of 2 on each side. These arrays are assigned from top to bottom, left to right.
 
-    #timer = time.time()
+    This is a key function for the MPI process, as it allows the rank 0 process to split and distribute dynamical data to each subprocess.
+    """
 
+    # finding the top left index of each rank
     rows, cols = arr.shape
     ind = np.where(ranks == rank)
     i = offset*ind[0][0]
@@ -197,12 +201,19 @@ def split(arr, offset, ranks, rank):
 
 
 def combine(mats, offset, ranks, size):
-    mats = np.array(mats[1:size+1])
+    """
+    Combines subarrays from each rank into one large array. Each rank calculates dynamics in each of its subarray, to get the large
+    array back for saving in the NETCDF output they need to be recombined on rank 0.
+    """
 
+    mats = np.array(mats[1:size+1]) # discard the rank 0 contribution
+
+    # line below discards the 2 cell padding around the entire subarray.
     matsnew = np.reshape(mats[:, 2:subdomain_size+2, :][:, :, 2:subdomain_size+2], (int(np.sqrt(size)),int(np.sqrt(size)),subdomain_size,subdomain_size))
 
-    mat = np.zeros((N,N))
+    mat = np.zeros((N,N)) # initialize full array
 
+    # add each subarray to the correct location in the new combined array, goes from top to bottom left to right by rank.
     for i in range(1,size+1):
         ind = np.where(ranks == i)
         mat[offset*ind[0][0] : offset*ind[0][0] + offset, :][:, offset*ind[1][0] : offset*ind[1][0] + offset] = matsnew[ind[0][0], ind[1][0]]
@@ -211,6 +222,12 @@ def combine(mats, offset, ranks, size):
 
 
 def get_surrounding_points(arr, i, j):
+    """
+    Returns the points surrounding a point in an array, with periodic boundary conditions. 
+    Used with MPI to find which ranks a cell has to communicate its dynamical data too to update subarray paddings.
+    """
+
+    # Array of relative index locations.
     surrounding_indices = [(-1, -1), (-1, 0), (-1, 1),
                            (0, -1),         (0, 1),
                            (1, -1), (1, 0), (1, 1)]
@@ -219,37 +236,10 @@ def get_surrounding_points(arr, i, j):
     surrounding_points = []
     just_ranks = []
     
+    # Appends the elements at each surrounding index, however with boundary wrapping.
     for di, dj in surrounding_indices:
-        ni, nj = (i + di) % rows, (j + dj) % cols
-        surrounding_points.append((ni-i, nj-j, arr[ni, nj]))
-        just_ranks.append(arr[ni,nj])
+        ni, nj = (i + di) % rows, (j + dj) % cols # Calculate the wrapped index
+        surrounding_points.append((ni-i, nj-j, arr[ni, nj])) # append the relative location (index) the rank
+        just_ranks.append(arr[ni,nj]) # append just the rank not the location.
     
-    return surrounding_points, set(just_ranks)
-
-"""
-Deprecated functions which might be needed later
-
-def split(mat, offset, ranks, rank):
-    
-    timer = time.time()
-    ind = np.where(ranks == rank)
-
-    temp = np.zeros((subdomain_size + 4, subdomain_size + 4))
-
-    left_padding = np.roll(mat, 2 , axis=1)[offset*ind[0][0] : offset*ind[0][0] + offset, :][:, offset*ind[1][0] : offset*ind[1][0] + 2]
-    right_padding = np.roll(mat, -2, axis=1)[offset*ind[0][0] : offset*ind[0][0] + offset, :][:, offset*ind[1][0] + offset - 2 : offset*ind[1][0] + offset]
-    top_padding = np.roll(mat, 2, axis=0)[offset*ind[0][0] : offset*ind[0][0] + 2, :][:, offset*ind[1][0] : offset*ind[1][0] + offset]
-    bot_padding = np.roll(mat, -2, axis=0)[offset*ind[0][0] + offset -2: offset*ind[0][0] + offset, :][:, offset*ind[1][0] : offset*ind[1][0] + offset]
-
-    temp[0:2, :][:, 2:len(temp)-2] = top_padding
-    temp[subdomain_size+2:subdomain_size+4, :][:, 2:len(temp)-2] = bot_padding
-    temp[2:len(temp)-2, :][:, 0:2] = left_padding
-    temp[2:len(temp)-2, : ][:, subdomain_size+2:subdomain_size+4] = right_padding
-
-    temp[2:subdomain_size+2, :][:, 2:subdomain_size+2] = mat[offset*ind[0][0] : offset*ind[0][0] + offset, :][:, offset*ind[1][0] : offset*ind[1][0] + offset]
-
-    print(time.time()-timer)
-
-    return temp
-
-"""
+    return surrounding_points, set(just_ranks) # return the relative locations of each rank, and a set of corresponding ranks.
